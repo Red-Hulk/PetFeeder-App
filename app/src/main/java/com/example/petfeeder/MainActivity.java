794d21d90838f9cc33925b0ae637a74370c1856f
Red-Hulk <te.meijer@ad-academie.nl>
2021-11-30 21:57:11 +0100

    Bottom-Navigation

--- app/src/main/java/com/example/petfeeder/MainActivity.java
+++ app/src/main/java/com/example/petfeeder/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.petfeeder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.petfeeder.fragments.CameraFragment;
+import com.example.petfeeder.fragments.FeedTimeFragment;
+import com.example.petfeeder.fragments.MenuFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.menu_option_feeder_time);
+
+    }
+    FeedTimeFragment feedTimeFragment = new FeedTimeFragment();
+    CameraFragment cameraFragment = new CameraFragment();
+    MenuFragment menuFragment = new MenuFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_option_feeder_time:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, feedTimeFragment).commit();
+                return true;
+
+            case R.id.menu_option_camera:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, cameraFragment).commit();
+                return true;
+
+            case R.id.menu_option_menu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, menuFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/petfeeder/fragments/FeedTimeFragment.java
@@ -0,0 +1,24 @@
+package com.example.petfeeder.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.*;
+import androidx.fragment.app.Fragment;
+
+import com.example.petfeeder.R;
+
+public class FeedTimeFragment extends Fragment {
+
+    public FeedTimeFragment(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed_time, container, false);
+    }
+}
